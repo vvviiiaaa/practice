f649424faa1a80450819077fa8c8aa7162b238d6
unknown <sergukhinvu@gmail.com>
2018-07-04 21:55:04 +0300

    Contollers classes added

---
+++ Proect/scr/controllers/edgeSizeDialogController.java
@@ -0,0 +1,99 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import objects.Edge;
+import objects.Vertex;
+
+import java.io.IOException;
+
+import static java.lang.Math.sqrt;
+
+/**
+ * Created by User on 02.07.2016.
+ */
+public class edgeSizeDialogController {
+    @FXML
+    public static TextField edgeSize;
+    public static double edgeWeight = -1;
+
+    private boolean isDigit(String text)
+    {
+        try{
+            int number = Integer.parseInt(text);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public void edgeSizeOK(ActionEvent actionEvent) {
+        System.out.println("Ok was pressed");
+        System.out.println("Text " + edgeSize.getText() + " was entered.");
+        String text = edgeSize.getText();
+        int weight;
+        if (isDigit(text)) {
+            weight = Integer.parseInt(text);
+            if (weight > 0) {
+                edgeWeight = (int) weight;
+                createEdgeFixedSize();
+                mainController.shouldGenerate = false;
+                mainController.generationType = true;
+                if (mainController.DelBut.isDisable())
+                    mainController.DelBut.setDisable(false);
+                if (mainController.ClearBut.isDisable())
+                    mainController.ClearBut.setDisable(false);
+                if (!mainController.AddArmBut.isDisable())
+                    mainController.AddArmBut.setDisable(true);
+                if (!mainController.RandGenBut.isDisable())
+                    mainController.RandGenBut.setDisable(true);
+                if (mainController.GotovoBut.isDisable())
+                    mainController.GotovoBut.setDisable(false);
+                if (!mainController.FatherBut.isDisable())
+                    mainController.FatherBut.setDisable(true);
+                if (!mainController.StepBut.isDisable())
+                    mainController.StepBut.setDisable(true);
+                mainController.activePane.getChildren().remove(mainController.edges.get(mainController.edges.size() - 1).txtWeight);
+                mainController.shouldBuild = true;
+                mainController.activePane.getChildren().add(mainController.edges.get(mainController.edges.size() - 1).txtWeight);
+                ((Stage) ((Node) actionEvent.getSource()).getScene().getWindow()).close();
+                //return n;
+            }
+            else {
+                edgeSize.clear();
+                edgeSize.setPromptText(" incorrect value ");
+            }
+            }
+            else
+            {
+
+                edgeSize.clear();
+                edgeSize.setPromptText(" incorrect value ");
+            }
+    }
+
+    public void edgeSizeCancel(ActionEvent actionEvent) {
+        System.out.println("Cancle was pressed");
+        mainController.edges.get(mainController.edges.size() - 1).isChecked = true;
+        mainController.deleteEdge();
+        ((Stage) ((Node) actionEvent.getSource()).getScene().getWindow()).close();
+    }
+
+    private void createEdgeFixedSize()  {
+        System.out.println("Create edge");
+        double weight = edgeWeight;
+        mainController.activePane.getChildren().remove(mainController.edges.get(mainController.edges.size() - 1).txtWeight);
+        mainController.edges.get(mainController.edges.size() - 1).setWeight(weight);
+        mainController.activePane.getChildren().add(mainController.edges.get(mainController.edges.size() - 1).txtWeight);
+        // Media sound = new Media(new File("src/content/Edge.wav").toURI().toString());
+        //MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        // mediaPlayer.play();
+    }
+
+}
---
+++ Proect/scr/controllers/generationDialogController.java
@@ -0,0 +1,114 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import objects.Edge;
+import objects.Vertex;
+
+/**
+ * Created by User on 29.06.2016.
+ */
+public class generationDialogController {
+
+    public static int vertexNumber = 0;
+
+    @FXML
+    TextField generationText;
+
+    private boolean isDigit(String text)
+    {
+        try{
+            int number = Integer.parseInt(text);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public void gdOK(ActionEvent actionEvent) {
+        System.out.println("Ok was pressed");
+        System.out.println("Text " + generationText.getText() + " was entered.");
+        String text = generationText.getText();
+        int n;
+        if (isDigit(text)) {
+            n = Integer.parseInt(text);
+            if (n > 0 && n < 8) {
+                vertexNumber = n;
+                mainController.shouldGenerate = false;
+                mainController.graphGenerationRandom();
+                mainController.generationType = true;
+                if (mainController.DelBut.isDisable())
+                    mainController.DelBut.setDisable(false);
+                if (mainController.ClearBut.isDisable())
+                    mainController.ClearBut.setDisable(false);
+                if (!mainController.AddArmBut.isDisable())
+                    mainController.AddArmBut.setDisable(true);
+                if (!mainController.RandGenBut.isDisable())
+                    mainController.RandGenBut.setDisable(true);
+                if (!mainController.FixedEdgesBut.isDisable())
+                    mainController.FixedEdgesBut.setDisable(true);
+                if (mainController.GotovoBut.isDisable())
+                    mainController.GotovoBut.setDisable(false);
+                if (!mainController.FatherBut.isDisable())
+                    mainController.FatherBut.setDisable(true);
+                if (!mainController.StepBut.isDisable())
+                    mainController.StepBut.setDisable(true);
+                mainController.shouldBuild = true;
+                ((Stage) ((Node) actionEvent.getSource()).getScene().getWindow()).close();
+                //return n;
+            }
+            else
+            {
+
+                generationText.clear();
+                generationText.setPromptText(" 1 < n < 8 ");
+                //return 0;
+            }
+        }
+        else
+        {
+            generationText.clear();
+            generationText.setPromptText(" 1 < n < 8 ");
+           //return 0;
+        }
+    }
+
+    public void gdCancel(ActionEvent actionEvent) {
+        System.out.println("Cancle was pressed");
+        mainController.shouldGenerate = false;
+        mainController.generationType = false;
+                ((Stage) ((Node) actionEvent.getSource()).getScene().getWindow()).close();
+    }
+
+    public static Vertex vertGeneration () {
+        ImageView image = new ImageView("content/drt1.png");
+        Vertex vertex = new Vertex(image);
+        double x;
+        double y;
+        boolean OK;
+        do {
+            OK = true;
+            x = Math.random() * 450;
+            y = Math.random() * 450;
+            for (Vertex item : mainController.buttons) {
+                double path = Math.sqrt((item.circle.getX() - x) * (item.circle.getX() - x)
+                        + (item.circle.getY() - y) * (item.circle.getY() - y));
+                if (path < 65)
+                    OK = false;
+            }
+        }
+        while (OK == false);
+        vertex.circle.setX(x);
+        vertex.circle.setY(y);
+        vertex.setNumber();
+        //mainController.buttons.add(vertex);
+        return vertex;
+    }
+
+}
